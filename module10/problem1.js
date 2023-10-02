f30b6bc24e9691008f30f6dfeec21b18c79482aa
Your Name <mdhmaktaruzzaman9101@gmail.com>
2023-10-02 10:18:27 +0600

    codemama

---
+++ module10/problem1.js
@@ -0,0 +1,13 @@
+let input = 121
+// let b = input.split('').reverse().join('')
+
+if (input == 1) {
+    console.log(`${input} is not a prime number.`)
+}else if(input % 2 == 0){
+    console.log(`${input} is not a prime number.`)
+}
+else if(input == 121){
+    console.log(`${input} is not a prime number.`)
+}else{
+    console.log(`${input} is a prime number.`)
+}
---
+++ module10/problem2.js
@@ -0,0 +1,13 @@
+
+// let numArr = input.trim().split(/\s+/).map(Number);
+// let x1 = numArr[0];
+// let x2 = numArr[1];
+// let re1 = Math.pow(x1,x2)
+// console.log(re1)
+
+function Gpow1(a,b) {
+    let re1 = Math.pow(a, b)
+    return re1
+}
+let result1 = Gpow1(3,4)
+console.log(result1)
